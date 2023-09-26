544bf7e6650f1172d3cb42a936d80f8829d19710
Trazeure <Destiny72002@outlook.com>
2023-09-26 17:19:37 -0600

    Primer commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Primo',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=['pytest'],
+    url='https://github.com/Trazeure/Primo.git',
+    author='Trazeure',
+    author_email='trazeure@hotmail.com'
+)
